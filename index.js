0ed3aa490b71de7f8bf17baa0eeac5d6a56f8e67
Aashish Karki <aashish.y2z@gmail.com>
2020-08-31 15:09:38 +0545

    update index

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const core = require('@actions/core');
 const { Octokit } = require('@octokit/rest');
 const { execSync } = require('child_process');
 
+const PERSONAL_ACCESS_TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
 const octokit = new Octokit({
-  auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+  auth: PERSONAL_ACCESS_TOKEN,
 });
 
 
-async function createRelease(repoName, name, body) {
+async function createRelease(repoName,tag, name, body) {
   await octokit.repos.createRelease({
     owner: 'karkipy',
     repo: repoName,
-    tag_name: 'next',
+    tag_name: tag,
     name,
     body,
     draft: true
@@ -30,27 +31,36 @@ try {
   // setup config for git
   execSync(`git config --global user.email action@bhoos.com`);
   execSync(`git config --global user.name 'Bhoos Action'`);
-
+  execSync(`git fetch origin`);
+  execSync('git config pull.ff only');
   // update the version
   execSync(`npm version ${branch} --no-git-tag-version`);
 
   const nextVersion = execSync(`node -p "require('./package.json').version"`).toString().trim();
-
   const repoName = execSync(`basename $(git remote get-url origin)`).toString().trim().split('.')[0];
-  execSync(`git fetch origin`);
-  execSync('git config pull.ff only');
-  execSync(`git tag next${nextVersion}`);
-  execSync('git push --tags');
-  execSync(`git checkout -b temp`);
-  execSync('git pull origin master')
+  const tagName = `next${nextVersion}`;
 
+
+
+  // add tags and push it
+  execSync(`git tag ${tagName}`);
+  execSync('git push --tags');
+
+  // push the updates from temp branch to both the current branch and master branch
+  execSync(`git checkout -b temp`);
+  execSync('git pull origin master');
   execSync(`git add -A`);
   execSync(`git commit -m "${branch} updated"`);
   execSync(`git push origin temp:${branch}`);
   execSync(`git push origin temp:master`);
-  createRelease(repoName, '', '').catch(e => {
-    console.log('e', e)
-  })
+  createRelease(repoName, tagName, '', '').catch(e => {
+    if (e) throw 'Draft Release error';
+  });
+
+  // setup to release package
+  execSync(`echo "//npm.pkg.github.com/rchs/:_authToken=${PERSONAL_ACCESS_TOKEN}" > ~/.npmrc`);
+  execSync(`echo "//npm.pkg.github.com/:_authToken=${PERSONAL_ACCESS_TOKEN}" >> ~/.npmrc`);
+  execSync(`npm publish`);
 
 }  catch (error) {
   core.setFailed(error.message);
